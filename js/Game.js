efa6369af6af174976d4b4a76adfca49cbaf2ebb
Spencer Christian <s.christian141@gmail.com>
2022-12-05 13:37:42 -0500

    Update Game.js

--- js/Game.js
+++ js/Game.js
@@ -1,18 +1,3 @@
-/* Treehouse FSJS Techdegree
- * Project 4 - OOP Game App
- * Game.js */
-
-/*-----------------------------------------------------------------------------
-○ missed: Used to track the number of missed guesses by the player. The initial
-value is `0`, since no guesses have been made at the start of the game.
-
-○ phrases: An array of Phrase objects to use with the game. For now, initialize the
-property to an empty array. In the next step you'll work on initializing this
-property with an array of Phrase objects.
-
-○ activePhrase: This is the Phrase object that’s currently in play. The initial value is
-`null`.
------------------------------------------------------------------------------*/ 
 
 const overlayTarget = document.querySelector("#overlay")
 class Game{
